d45355fc76024ba5247223c41beedea48c96050f
molnarland <rrmolnar@gmail.com>
2017-12-07 20:39:40 +0100

    init

---
+++ src/init.cpp
@@ -0,0 +1,12 @@
+#include "oxygine-framework.h"
+#include "init.h"
+
+using namespace oxygine;
+
+void example_preinit () {}
+
+void example_init () {}
+
+void example_update() {}
+
+void example_destroy() {}
